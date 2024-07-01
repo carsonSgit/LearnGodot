8e1a14bee394c776726f82b619e76151dc26c6f4
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-07-01 11:21:45 -0400

    TileMap & A/D movement

---
+++ scripts/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED = 120.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# Use custom gameplay actions.
+	var direction = 0
+	if Input.is_action_pressed("move_left"):
+		direction -= 1
+	if Input.is_action_pressed("move_right"):
+		direction += 1
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0  # Stop immediately if no directional input
+
+	move_and_slide()
